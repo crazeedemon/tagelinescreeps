94f4805b2023e3b0166dec7d73febd11f13aef36
crazeedemon <daniel_pretorius@live.com>
2019-12-21 10:14:04 +1300

    updated code

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,60 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
 module.exports.loop = function () {
-    // Your code goes here
+
+    var tower = Game.getObjectById('TOWER_ID');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+        // goal: have 10 harvesters and as many upgraders as possible
+        var minimumNumberOfHarvesters = 10;
+        // _.sum will count the number of properties in Game.creeps filtered by the
+        //  arrow function, which checks for the creep being a harvester
+        var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
+        var name = undefined;
+
+        // if not enough harvesters
+        if (numberOfHarvesters < minimumNumberOfHarvesters) {
+            // try to spawn one
+            name = Game.spawns.Crazeedemon.createCreep([WORK,WORK,CARRY,MOVE], undefined,
+                { role: 'harvester', working: false});
+        }
+        else {
+            // else try to spawn an upgrader
+            // small change from what you saw in the video: for upgraders it makes
+            //  more sense to have two move parts because they have to travel further
+            name = Game.spawns.Crazeedemon.createCreep([WORK,CARRY,MOVE,MOVE], undefined,
+                { role: 'upgrader', working: false});
+        }
+
+        // print name to console if spawning was a success
+        // name > 0 would not work since string > 0 returns false
+        if (!(name < 0)) {
+            console.log("Spawned new creep: " + name);
+        }
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,82 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
 module.exports.loop = function () {
-    // Your code goes here
+
+    var tower = Game.getObjectById('TOWER_ID');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+        // goal: have 5 harvesters and as many upgraders as possible
+        var minimumNumberOfHarvesters = 5;
+        // _.sum will count the number of properties in Game.creeps filtered by the
+        //  arrow function, which checks for the creep being a harvester
+        var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
+        var name = undefined;
+        var _type = ''
+        // goal have 5 upgraders
+        var minimumNumberOfUpgraders = 3;
+        // _.sum will count the number of properties in Game.creeps filtered by the
+        //  arrow function, which checks for the creep being a harvester
+        var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
+
+        // goal have 5 builders
+        var minimumNumberOfbuilders = 2;
+        // _.sum will count the number of properties in Game.creeps filtered by the
+        //  arrow function, which checks for the creep being a harvester
+        var numberOfbuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
+
+        // if not enough harvesters
+        if (numberOfHarvesters < minimumNumberOfHarvesters) {
+            // try to spawn one
+            _type = 'harvester'
+            name = Game.spawns.Crazeedemon.createCreep([WORK,WORK,CARRY,MOVE], undefined,
+                { role: 'harvester', working: false});
+        }
+        if (numberOfUpgraders < minimumNumberOfUpgraders)  {
+            // else try to spawn an upgrader
+            // small change from what you saw in the video: for upgraders it makes
+            //  more sense to have two move parts because they have to travel further
+            _type = 'upgrader'
+            name = Game.spawns.Crazeedemon.createCreep([WORK,CARRY,MOVE,MOVE], undefined,
+                { role: 'upgrader', working: false});
+        }
+        if (numberOfbuilders < minimumNumberOfbuilders)  {
+            // else try to spawn an upgrader
+            // small change from what you saw in the video: for upgraders it makes
+            //  more sense to have two move parts because they have to travel further
+            _type = 'builder'
+            name = Game.spawns.Crazeedemon.createCreep([WORK,CARRY,MOVE,MOVE], undefined,
+                { role: 'builder', working: false});
+        }
+
+        // print name to console if spawning was a success
+        // name > 0 would not work since string > 0 returns false
+        if (!(name < 0)) {
+            console.log("Spawned new creep: " + name + _type);
+        }
+    }
 }
